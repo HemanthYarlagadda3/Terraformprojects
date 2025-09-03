8af51772be51a289c8c879857caf485811d2cadc
HemanthYarlagadda3 <yarl.hemanth@gmail.com>
2025-09-03 17:49:01 +0530

    Fix project01: track files instead of submodule

---
+++ project01/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-0c4fc5dcabc9df21d"
+  instance_type = "t3.micro"
+  key_name      = "LUNUX@2"
+  subnet_id     = "subnet-0cbe0b341fabc2276"
+  tags = {
+    Name = "Test"
+  }
+}
+
